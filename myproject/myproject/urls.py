38e421be2b651e1719e11b82224d286b3b6cfea1
mschettler <mschettler@gmail.com>
2012-12-30 19:42:34 -0800

    split urls.py to use properly use both the app/global level hybrid urls.py scheme

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,17 +1,25 @@
+# project wide urls
 from django.conf.urls import patterns, include, url
+from django.views.generic.simple import redirect_to
+from django.core.urlresolvers import reverse
+from django.contrib import admin
+admin.autodiscover()
+import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+# import your urls from each app here, as needed
+import myfirstapp.urls
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'myfirstapp.views.home'),
-    # url(r'^myproject/', include('myproject.foo.urls')),
+
+    # urls specific to this app
+    url(r'^myfirstapp/', include(myfirstapp.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+
+    # catch all, redirect to myfirstapp home view
+    url(r'.*', redirect_to, {'url': '/myfirstapp/home'}),
+
 )
